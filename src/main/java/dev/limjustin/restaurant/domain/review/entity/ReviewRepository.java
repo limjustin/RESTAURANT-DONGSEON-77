013a1a6ed5ceca66d62306e4cb748932fc657bf7
Jaeyoung Lim <limjyjustin@gmail.com>
2024-03-06 11:32:40 +0900

    add: 리뷰 엔티티 생성

---
+++ src/main/java/dev/limjustin/restaurant/domain/review/entity/ReviewRepository.java
@@ -0,0 +1,7 @@
+package dev.limjustin.restaurant.domain.review.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
---
+++ src/main/java/dev/limjustin/restaurant/global/enums/Rating.java
@@ -0,0 +1,5 @@
+package dev.limjustin.restaurant.global.enums;
+
+public enum Rating {
+    ONE, TWO, THREE, FOUR, FIVE;
+}
