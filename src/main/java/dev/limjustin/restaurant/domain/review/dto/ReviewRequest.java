a1f4a420c556dedf69c01f492ca1f4d0882f1769
Jaeyoung Lim <limjyjustin@gmail.com>
2024-03-06 15:24:45 +0900

    chore: Thymeleaf 화면 제작

--- src/main/java/dev/limjustin/restaurant/domain/review/dto/ReviewRequest.java
+++ src/main/java/dev/limjustin/restaurant/domain/review/dto/ReviewRequest.java
@@ -13,6 +13,17 @@ public class ReviewRequest {
     @Data @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    public static class FormDto {
+        private String writer;
+        private String imageLink;  // 사진 링크 (AWS S3)
+        private Rating rating;
+        private String comment;
+        private List<Keyword> keywords;
+    }
+
+    @Data @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class PostDto {
         private String writer;
         private String imageLink;  // 사진 링크 (AWS S3)
--- src/main/java/dev/limjustin/restaurant/domain/review/web/ReviewController.java
+++ src/main/java/dev/limjustin/restaurant/domain/review/web/ReviewController.java
@@ -2,19 +2,44 @@ package dev.limjustin.restaurant.domain.review.web;
 
 import dev.limjustin.restaurant.domain.review.dto.ReviewRequest;
 import dev.limjustin.restaurant.domain.review.service.ReviewService;
-import lombok.Getter;
+import dev.limjustin.restaurant.global.enums.Keyword;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @GetMapping("/api/v1/post")
+    public String createPostForm(Model model) {
+        model.addAttribute("formDto", new ReviewRequest.FormDto());
+        return "review/post";
+    }
+
     @PostMapping("/api/v1/post")
-    public ResponseEntity<?> post(@RequestBody ReviewRequest.PostDto postDto) {
+    public ResponseEntity<?> post(@ModelAttribute("formDto") ReviewRequest.FormDto formDto) {
+
+        System.out.println("formDto.getKeywords() = " + formDto.getKeywords());
+
+        List<Keyword> keywords = new ArrayList<>(formDto.getKeywords());
+
+        ReviewRequest.PostDto postDto = ReviewRequest.PostDto.builder()
+                .writer(formDto.getWriter())
+                .rating(formDto.getRating())
+                .comment(formDto.getComment())
+                .imageLink("string")
+                .keywords(keywords)
+                .build();
+
         return ResponseEntity.ok(reviewService.post(postDto));
     }
 
